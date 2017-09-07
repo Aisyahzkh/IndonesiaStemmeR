a1703f3a4948669e429b4f9119707b1c75641352
Bukalapak <aisyahzkh14@gmail.com>
2017-09-07 09:32:03 +0700

    add kamus to parameter

--- R/is_katadasar.R
+++ R/is_katadasar.R
@@ -1,5 +1,12 @@
-is_katadasar <- function(kata) {
-  kata_dasar <- unique(kamus_katadasar)
+is_katadasar <- function(kata, kamus=NULL) {
+  if ( is.null(kamus) ) {
+    kata_dasar <- unique(kamus_katadasar)
+  } else if ( is.vector(kamus) ) {
+    kata_dasar <- unique(c(kamus_katadasar, kamus))
+  } else {
+    stop("Kamus harus berupa vektor!")
+  }
+
   b <- !is.na(match(kata, kata_dasar))
   return(b)
 }
